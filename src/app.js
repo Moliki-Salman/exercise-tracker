dfd6c369d6a86488e2dbca03479acf4fc391bf13
Moliki-Salman <salmanameenat@gmail.com>
2023-09-21 12:06:56 +0200

    app.js setup

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express();
+const mongoose = require("mongoose")
+
+
+const connectToDB = mongoose.connect('mongodb://127.0.0.1:27017/ExerciceTracker')
+.then(() => {
+  console.log("Connected to mongoBD database")
+})
+
+app.listen(5000, () => {
+  console.log("Server is running on Port 5000")
+})
